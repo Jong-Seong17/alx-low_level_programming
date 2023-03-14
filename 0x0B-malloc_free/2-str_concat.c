578d11341020e1fd29b07d9de33e7a4e15fce8a7
Jong-Seong <jongseong.amari@gmail.com>
2023-03-14 05:59:05 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,6 +9,7 @@
 */
 
 char *str_concat(char *s1, char *s2)
+
 {
 	char *conct;
 	int i, ci;
@@ -18,31 +19,28 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 		s2 = "";
-
 		i = ci = 0;
+
 	while (s1[i] != '\0')
 		i++;
 
 	while (s2[ci] != '\0')
 		ci++;
-
 	conct = malloc(sizeof(char) * (i + ci + 1));
+
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
 
 	while (s1[i] != '\0')
-
 	{
-	conct[i] = s1[i];
+		conct[i] = s1[i];
 		i++;
 	}
 
 	while (s2[ci] != '\0')
-
 	{
 		conct[i] = s2[ci];
-
 		i++, ci++;
 	}
 	conct[i] = '\0';
